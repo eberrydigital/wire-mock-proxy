48e0f1831c036d47ec907a342f067457b7ed6646
atsoy <anatoli.tsoi@strawberry.se>
2025-10-14 13:54:27 +0200

    moved bootstrapping to ServerBootstrap.kt

---
+++ app/src/main/kotlin/se/strawberry/app/ServerBootstrap.kt
@@ -0,0 +1,112 @@
+package se.strawberry.app
+
+
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
+import com.github.tomakehurst.wiremock.core.WireMockConfiguration.options
+import org.slf4j.LoggerFactory
+import se.strawberry.admin.ServerRef
+import se.strawberry.api.RequestsApiTransformer
+import se.strawberry.common.Headers.X_MOCK_TARGET_SERVICE
+import se.strawberry.common.Json
+import se.strawberry.common.Paths.API_PREFIX
+import se.strawberry.common.Paths.UI_ASSETS_PREFIX
+import se.strawberry.common.Paths.UI_ROOT
+import se.strawberry.common.Priorities.PROXY_FALLBACK
+import se.strawberry.common.Priorities.UI
+import se.strawberry.common.TransformerNames
+import se.strawberry.config.EnvironmentConfig
+import se.strawberry.config.ServiceRegistry
+import se.strawberry.extensions.filters.DynamicRoutingGuard
+import se.strawberry.extensions.listeners.EphemeralServeEventListener
+import se.strawberry.extensions.matchers.TtlGuardMatcher
+import se.strawberry.extensions.templating.ServiceTemplateHelpers
+import se.strawberry.extensions.transformers.UpstreamPatchTransformer
+
+
+object ServerBootstrap {
+    private val log = LoggerFactory.getLogger(ServerBootstrap::class.java)
+    val mapper = Json.mapper
+
+    fun start(): WireMockServer {
+
+        val cfg = EnvironmentConfig
+        val services = ServiceRegistry.fromEnv()
+
+        val server = WireMockServer(
+            options()
+                .port(cfg.port)
+                .bindAddress(cfg.bindAddress)
+                .usingFilesUnderDirectory("__files")  // Ui Files reside there
+                .extensions(
+                    DynamicRoutingGuard(services),
+                    TtlGuardMatcher(),
+                    UpstreamPatchTransformer(mapper),
+                    RequestsApiTransformer(),
+                    EphemeralServeEventListener(),
+                    ServiceTemplateHelpers(services)
+                )
+                .templatingEnabled(true)
+        )
+
+        server.start()
+        ServerRef.server = server
+        log.info("WireMock proxy started on {}:{}; services: {}", cfg.bindAddress, cfg.port, services)
+
+        // Ui Files
+        registerRulesForFrontendRequests(server)
+
+        server.stubFor(
+            any(urlPathMatching("$API_PREFIX/.*")).atPriority(UI)
+                .willReturn(
+                    aResponse()
+                        .withTransformers(TransformerNames.REQUESTS_API)
+                )
+        )
+
+        server.stubFor(
+            any(urlMatching(".*")).atPriority(PROXY_FALLBACK)
+                .willReturn(
+                    aResponse()
+                        .proxiedFrom("{{service-origin name=request.headers.[$X_MOCK_TARGET_SERVICE]}}")
+                        .withTransformers("response-template")
+                )
+        )
+
+        Runtime.getRuntime().addShutdownHook(Thread { server.stop() })
+        return server
+    }
+
+    private fun registerRulesForFrontendRequests(server: WireMockServer) {
+        server.stubFor(
+            get(urlEqualTo(UI_ROOT)).atPriority(UI)
+                .willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", "text/html; charset=utf-8")
+                        .withHeader("Cache-Control", "no-store")
+                        .withBodyFile("ui/index.html")
+                )
+        )
+
+        server.stubFor(
+            get(urlPathMatching("$UI_ASSETS_PREFIX/.*")).atPriority(UI)
+                .willReturn(
+                    aResponse()
+                        .withHeader("Cache-Control", "no-store")
+                        .withTransformerParameter("pathPrefix", "ui/assets")
+                        .withBodyFile("ui/assets/styles.css")
+                )
+        )
+
+        server.stubFor(
+            get(urlEqualTo("$UI_ASSETS_PREFIX/app.js")).atPriority(UI)
+                .willReturn(
+                    aResponse()
+                        .withHeader("Content-Type", "application/javascript; charset=utf-8")
+                        .withHeader("Cache-Control", "public, max-age=31536000, immutable")
+                        .withBodyFile("ui/assets/app.js")
+                )
+        )
+
+    }
+}
